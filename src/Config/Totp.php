85175ca8baaa7b792225a7f210317aa6b67221c2
grimpirate <grimpirate_jrs@yahoo.com>
2026-02-08 22:39:36 -0500

    Version updates and fixes for 32-bit secret

--- src/Config/Totp.php
+++ src/Config/Totp.php
@@ -7,5 +7,5 @@ use CodeIgniter\Config\BaseConfig;
 class Totp extends BaseConfig
 {
     public string $issuer       = 'Halberd';
-    public int $secretKeyLength = 16;
+    public int $secretKeyLength = 32; // Updated to reflect https://github.com/antonioribeiro/google2fa/releases/tag/v9.0.0
 }
