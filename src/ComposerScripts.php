39c40daaf0b31e88aa36806a81adfd33194a0376
grimpirate <grimpirate_jrs@yahoo.com>
2026-02-10 21:16:26 -0500

    Update ComposerScripts.php

--- src/ComposerScripts.php
+++ src/ComposerScripts.php
@@ -2,34 +2,43 @@
 
 namespace GrimPirate\Halberd;
 
-final class ComposerScripts
+class ComposerScripts
 {
-	public static function postUpdate(array $params)
+	public static function postUpdate(): void
 	{
-		echo __DIR__;
+		$notSrc = array_filter(self::listAllFiles(__DIR__ . DIRECTORY_SEPARATOR . '../'), function($path) {
+			if(1 === preg_match('/^.*\/grimpirate\/halberd\/composer.json$/', $path)) return false;
+			if(1 === preg_match('/^.*\/grimpirate\/halberd\/src.*$/', $path)) return false;
+			return true;
+		});
+
+		usort($notSrc, function($pathA, $pathB) {
+			if(is_file($pathA) && is_dir($pathB)) return -1;
+			if(is_dir($pathA) && is_file($pathB)) return 1;
+			if(is_dir($pathA) && is_dir($pathB)) return strlen($pathA) < strlen($pathB) ? 1 : -1;
+			return 0;
+		});
+
+		foreach($notSrc as $path)
+			if(is_file($path))
+				unlink($path);
+			else
+				rmdir($path);
 	}
 
-	public static function removeDir(string $dir): void
+	public static function listAllFiles($dir): array
 	{
-		// Create RecursiveDirectoryIterator with SKIP_DOTS flag to ignore '.' and '..'
-		$it = new \RecursiveDirectoryIterator($dir, \RecursiveDirectoryIterator::SKIP_DOTS);
-		// Create RecursiveIteratorIterator with CHILD_FIRST flag to ensure child items are processed first
-		$files = new \RecursiveIteratorIterator($it, \RecursiveIteratorIterator::CHILD_FIRST);
+		$array = array_diff(scandir($dir), array('.', '..'));
 
-		foreach($files as $file)
-		{
-			if ($file->isDir())
-			{
-				// Remove directory
-				rmdir($file->getPathname());
-			}
-			else
-			{
-				// Delete file
-				unlink($file->getPathname());
-			}
-		}
-		// Remove the main directory after its contents are empty
-		rmdir($dir);
+		foreach($array as &$item)
+			$item = realpath($dir . $item);
+
+		unset($item);
+
+		foreach($array as $item)
+			if(is_dir($item))
+				$array = array_merge($array, self::listAllFiles($item . DIRECTORY_SEPARATOR));
+
+		return $array;
 	}
 }
